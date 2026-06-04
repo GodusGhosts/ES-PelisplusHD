2c74c588d65f42ce3f76d8124675692c44015741
GodusGhosts <gruiz172@gmail.com>
2026-06-03 21:18:27 -0400

    Initialize build.gradle.kts for Android library

---
+++ MiProvider/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.mi.repo"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 21
+    }
+}
+
+dependencies {
+}
